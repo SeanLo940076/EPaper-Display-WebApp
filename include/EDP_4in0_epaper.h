f7e14ed2eb70295f325d44318ed99fd75a9e03c5
Sean Lo <127285997+SeanLo940076@users.noreply.github.com>
2025-02-25 14:44:31 +0800

    Add EDP_7in3 lib

---
+++ include/EDP_4in0_epaper.h
@@ -0,0 +1,22 @@
+#ifndef EDP_4IN0_EPAPER_H
+#define EDP_4IN0_EPAPER_H
+
+extern "C" {
+    #include "lib/e-Paper/EPD_4in0e.h"
+  }
+
+// 初始化 e-Paper 模組，若失敗回傳 false
+bool DEV_init_epaper();
+
+// Initialize the e-Paper register
+void init_epaper();
+
+void display_epaper(UBYTE* imgBuf);
+
+// 清除 e-Paper 畫面（清白後進入休眠）
+void clear_epaper();
+
+// 程式結束前的清理動作（進入休眠並釋放模組）
+void cleanup_epaper();
+
+#endif // EDP_4IN0_EPAPER_H
---
+++ src/EDP_7in3_epaper.cpp
@@ -0,0 +1,39 @@
+#include "epaper.h"
+#include <iostream>
+ 
+// 包含 e-Paper 所需的 C 語言標頭檔，注意依照你的目錄結構調整路徑
+extern "C" {
+  #include "lib/Config/DEV_Config.h"
+  #include "lib/e-Paper/EPD_7in3e.h"
+  #include "lib/GUI/GUI_Paint.h"
+}
+
+bool DEV_init_epaper() {
+    if (DEV_Module_Init() != 0) {
+        std::cerr << "DEV_Module_Init failed\n";
+        return false;
+    }
+    return true;
+}
+
+void init_epaper() {
+    EPD_7IN3E_Init();
+}
+
+void clear_epaper() {
+    EPD_7IN3E_Init();
+    EPD_7IN3E_Clear(EPD_7IN3E_WHITE);
+    // EPD_7IN3E_Show7Block();
+    EPD_7IN3E_Sleep();
+}
+
+void display_epaper(UBYTE* imgBuf) {
+    EPD_7IN3E_Init();
+    EPD_7IN3E_Display(imgBuf);
+    EPD_7IN3E_Sleep();
+}
+
+void cleanup_epaper() {
+    EPD_7IN3E_Sleep();
+    DEV_Module_Exit();
+}
